27ffa98cce4c33ed49acf6e31ea9a1262257ed69
Eduardo Wagner <eduardoodelonw@gmail.com>
2023-09-08 01:15:22 -0300

    feat(users): add update endpoint

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Patch } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from 'src/common/decorators/user.decorator';
 import { JwtUserPayload } from 'src/common/types/user-payload.type';
 import { CreateUserDto } from './dto/create-user-data.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -18,5 +19,14 @@ export class UsersController {
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
     return await this.service.create(createUserDto);
+  }
+
+  @Patch()
+  @UseGuards(JwtAuthGuard)
+  async update(
+    @User() user: JwtUserPayload,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return await this.service.updateById(user.id, updateUserDto);
   }
 }
